7a9d99e7e9c96523ec67a7e3461ed4c70ef9bed1
Thibaud Ducasse <11507599+tducasse@users.noreply.github.com>
2023-10-24 13:19:27 +0100

    Update config.lua

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -17,12 +17,6 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 
--- COPILOT
-require("copilot").setup({
-	suggestion = { enabled = false },
-	panel = { enabled = false },
-})
-
 -- TREESITTER
 require("nvim-treesitter.configs").setup({
 	highlight = {
@@ -37,6 +31,7 @@ require("nvim-treesitter.configs").setup({
 		"python",
 		"bash",
 		"rust",
+		"ruby",
 		"html",
 		"json",
 		"lua",
@@ -47,8 +42,20 @@ require("nvim-treesitter.configs").setup({
 	},
 })
 
+-- COPILOT
+require("copilot").setup({
+	suggestion = { enabled = false },
+	panel = { enabled = false },
+})
+
 -- BUFFERLINE
-require("bufferline").setup({})
+require("bufferline").setup({
+	options = {
+		separator_style = "slant",
+		offsets = { { filetype = "NvimTree", text = "File Explorer", text_align = "center" } },
+	},
+})
+vim.keymap.set("n", "<leader>b", ":BufferLinePick<CR>")
 
 -- NVIMTREE
 vim.g.loaded_netrw = 1
@@ -58,100 +65,10 @@ require("nvim-tree").setup({
 		ignore = false,
 	},
 })
-vim.keymap.set("n", "<leader>e", ":NvimTreeFocus<CR>")
+vim.keymap.set("n", "<leader>e", ":NvimTreeFindFile<CR>")
 
 -- LUALINE
 require("lualine").setup()
-
--- LSP
-local lsp = require("lsp-zero")
-lsp.preset("recommended")
-lsp.ensure_installed({
-	"tsserver",
-	"eslint",
-	"lua_ls",
-	"rust_analyzer",
-	"jedi_language_server",
-	"gopls",
-})
-lsp.nvim_workspace()
-lsp.set_preferences({
-	set_lsp_keymaps = false,
-})
-lsp.setup()
-local cmp = require("cmp")
-cmp.setup({
-	sources = {
-		{ name = "copilot" },
-		{ name = "path" },
-		{ name = "nvim_lsp" },
-		{ name = "buffer", keyword_length = 3 },
-		{ name = "luasnip", keyword_length = 2 },
-	},
-	experimental = {
-		ghost_text = {
-			hl_group = "Comment",
-		},
-	},
-	mapping = {
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-	},
-})
-vim.cmd([[autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js EslintFixAll]])
-
--- LSP BINDINGS
-require("lspsaga").setup({
-	code_action_lightbulb = {
-		enable = false,
-		sign = false,
-		virtual_text = false,
-	},
-})
-vim.keymap.set("n", "gr", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
-vim.keymap.set({ "n", "v" }, "gf", "<cmd>Lspsaga code_action<CR>", { silent = true })
-vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { silent = true })
-vim.keymap.set("n", "gx", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
-vim.keymap.set("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
-vim.keymap.set("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
-vim.keymap.set("n", "gh", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
-
--- MASON TOOLS
-require("mason-tool-installer").setup({
-	ensure_installed = {
-		"black",
-		"stylua",
-		"flake8",
-		"isort",
-		"goimports",
-	},
-})
-
--- NULL-LS
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-local null_ls = require("null-ls")
-null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.black.with({
-			extra_args = { "-l", "80" },
-		}),
-		null_ls.builtins.formatting.isort,
-		null_ls.builtins.diagnostics.flake8,
-		null_ls.builtins.formatting.goimports,
-	},
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = bufnr })
-				end,
-			})
-		end
-	end,
-})
 
 -- FUGITIVE
 vim.cmd([[
@@ -178,3 +95,90 @@ require("Comment").setup()
 
 -- AUTOPAIRS
 require("nvim-autopairs").setup()
+
+-- LSP
+local lsp = require("lsp-zero")
+lsp.preset("recommended")
+lsp.ensure_installed({
+	"lua_ls",
+	"tsserver",
+	"solargraph",
+	"yamlls",
+	"jedi_language_server",
+	"eslint",
+})
+lsp.nvim_workspace()
+lsp.set_preferences({
+	set_lsp_keymaps = false,
+})
+lsp.setup()
+local cmp = require("cmp")
+cmp.setup({
+	sources = {
+		{ name = "copilot" },
+		{ name = "path" },
+		{ name = "nvim_lsp" },
+		{ name = "buffer", keyword_length = 3 },
+		{ name = "luasnip", keyword_length = 2 },
+	},
+	experimental = {
+		ghost_text = {
+			hl_group = "Comment",
+		},
+	},
+	mapping = {
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+	},
+})
+
+-- LSP BINDINGS
+require("lspsaga").setup({
+	code_action_lightbulb = {
+		enable = false,
+		sign = false,
+		virtual_text = false,
+	},
+})
+vim.keymap.set("n", "gr", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
+vim.keymap.set({ "n", "v" }, "gf", "<cmd>Lspsaga code_action<CR>", { silent = true })
+vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { silent = true })
+vim.keymap.set("n", "gx", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
+vim.keymap.set("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
+vim.keymap.set("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
+vim.keymap.set("n", "gh", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
+
+-- MASON TOOLS
+require("mason-tool-installer").setup({
+	ensure_installed = {
+		"stylua",
+		"eslint_d",
+	},
+})
+
+-- FORMATTING
+require("formatter").setup({
+	logging = false,
+	filetype = {
+		lua = {
+			require("formatter.filetypes.lua").stylua,
+		},
+		javascript = {
+			require("formatter.filetypes.javascript").eslint_d,
+		},
+		javascriptreact = {
+			require("formatter.filetypes.javascript").eslint_d,
+		},
+		typescript = {
+			require("formatter.filetypes.javascript").eslint_d,
+		},
+		typescriptreact = {
+			require("formatter.filetypes.javascript").eslint_d,
+		},
+	},
+})
+vim.cmd([[
+  augroup FormatAutogroup
+    autocmd!
+    autocmd BufWritePost * FormatWrite
+  augroup END
+]])
